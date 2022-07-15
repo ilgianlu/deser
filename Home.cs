1a6c6829a58459234ca8510fa36eb9dd867eba9b
Gianluca Magni <magni.c-@legrand-ext.com>
2022-07-15 14:42:52 +0200

    first commit

---
+++ Home.cs
@@ -0,0 +1,30 @@
+﻿namespace Deser;
+
+using System.Text.Json;
+public class Home
+{
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+
+    public string toJson()
+    {
+        //var options = new JsonSerializerOptions { WriteIndented = true };
+        var options = new JsonSerializerOptions {};
+        return JsonSerializer.Serialize(this, options);
+    }
+
+    public static Home? fromJson(string homeAsString)
+    {
+        Home? home = null;
+        try
+        {
+            home = JsonSerializer.Deserialize<Home>(homeAsString);
+            return home;
+        }
+        catch
+        {
+            Console.WriteLine("Exception thrown");
+            return home;
+        }
+    }
+}
